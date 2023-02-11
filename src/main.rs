d0b0ff871f8cdc48c7857ffb1499d0e48100ece7
Adam Rao <raoadam87@gmail.com>
2023-02-11 20:29:57 +0300

    Read from a file
    Add code to read from filepath passed on as command line argument.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let query: &String = &args[1];
     let file_path: &String = &args[2];
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let content: String = fs::read_to_string(file_path).expect("File not found");
+
+    println!("Text:\n {content}")
 }
