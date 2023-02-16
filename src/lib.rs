bf17db73ee9dfb3e051e6501e3d7cb311d5a3e61
Adam Rao <raoadam87@gmail.com>
2023-02-16 20:12:59 +0300

    Add unit test

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,35 @@ impl Data {
 pub fn run(data: Data) -> Result<(), Box<dyn Error>>{
     let content: String = fs::read_to_string(data.file_path)?;
 
-    println!("Text:\n {content}");
+    for line in search(&data.query, &content) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+           results.push(line) 
+        }
+    }
+    
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "thing";
+
+        let contents = "Something something darkside";
+
+        assert_eq!(vec!["Something something darkside"], search(query, contents))
+    }
 }
